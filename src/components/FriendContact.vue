5a37e64f526378d17cdd4b1ec00d625bf6658f96
Pholist <64503848+Pholist@users.noreply.github.com>
2021-03-02 10:06:37 +0100

    edit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,39 @@
+<template>
+  
+  <li>
+    <h2 @click="toggleFavorite">{{ name }} {{isFavorite ? 'favorite' : ''}}</h2>
+    <button @click="toggleDetails">Details</button>
+    <button @click="$emit('delete',id)">Delete</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+  
+</template>
+
+<script>
+export default {
+ name:"FriendContact",
+ props: ["name", "phoneNumber", "emailAddress","isFavorite","id"],
+  data(){
+     return{
+       detailsAreVisible:false,
+     };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite(){
+    this.$emit('toggle-favorite',this.id)
+    }
+  }
+};
+</script>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="enteredName" />
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="tel" v-model="enteredPhone" />
+    </div>
+    <div>
+      <label>E-Mail</label>
+      <input type="email" v-model="enteredAddress" />
+    </div>
+    <div>
+      <button>Add Contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "NewFriend",
+  data() {
+    return {
+      enteredName: "",
+      enteredAddress: "",
+      enteredPhone: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit(
+        "submit-data",
+        this.enteredName,
+        this.enteredPhone,
+        this.enteredAddress
+      );
+    },
+  },
+};
+</script>
